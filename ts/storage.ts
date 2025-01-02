db0d89b3ecd949c7149e3b0e9a174ec411379811
teatime77 <teatime77@live.jp>
2025-01-02 18:35:33 +0900

    uploadCanvasImg

--- ts/storage.ts
+++ ts/storage.ts
@@ -6,6 +6,34 @@ export function initStorage(){
     msg(`storage:${storage}`);
 }
 
+function getThumbnailPath(doc_id : number) : string {
+    return `users/${refId}/images/${doc_id}/thumbnail.png`;
+}
+
+export async function uploadCanvasImg(doc_id : number, canvas : HTMLCanvasElement) {
+    // Get canvas data as a Blob
+    const dataURL = canvas.toDataURL('image/png'); 
+    const res  = await fetch(dataURL);
+    const blob = await res.blob();
+
+    try{
+        // Create a root reference
+        var storageRef = firebase.storage().ref();
+
+        // Create a reference to 'images/mountains.jpg'
+        const path = getThumbnailPath(doc_id);
+        var img_ref = storageRef.child(path);
+
+        const snap = await img_ref.put(blob);
+        msg(`upload canvas img OK: path:${path}`);
+
+        return path;
+    }
+    catch(e){
+        throw new MyError(`upload canvas img err:${e}`);
+    }
+}
+
 export async function uploadImgFile(file : File) {
     const k = file.type.indexOf("/");
     const ext = file.type.substring(k + 1);
