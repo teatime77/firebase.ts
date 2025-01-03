0c70f34c90e767f7d45cc0c863c6ec57d3da496a
teatime77 <teatime77@live.jp>
2025-01-03 09:41:22 +0900

    add graphviz, edge.ts, graph.ts

---
+++ ts/graph/edge.ts
@@ -0,0 +1,477 @@
+namespace firebase_ts {
+
+let edge_Map = new Map<string, Edge>();
+let selectedDoc : Doc | null = null;
+
+export function edgeKey(doc1 : Doc, doc2 : Doc) : string {
+    return `${doc1.id}:${doc2.id}`;
+}
+
+export function getEdge(edge_map : Map<string, Edge>, doc1 : Doc, doc2 : Doc) : Edge | undefined {
+    const key = edgeKey(doc1, doc2);
+    const edge = edge_map.get(key)!;
+
+    return edge;
+}
+
+export function addEdge(edge_map : Map<string, Edge>, src_doc : Doc, dst_doc : Doc) : Edge {
+    src_doc.dsts.push(dst_doc);
+    dst_doc.srcs.push(src_doc);
+
+    const edge = new Edge(src_doc, dst_doc);
+    const key = edgeKey(src_doc, dst_doc);
+    edge_map.set(key, edge);
+
+    return edge;
+}
+
+function cancelLinkDocs(){
+    if(selectedDoc != null){
+
+        selectedDoc.setColor("black");
+        selectedDoc = null;
+    }
+}
+
+function makeDocsFromDbFolder(docs : Doc[], folder : DbFolder) : any {
+    // const obj : { docs : }
+    for(const item of folder.children){
+        if(item instanceof DbFolder){
+            makeDocsFromDbFolder(docs, item);
+        }
+        else if(item instanceof DbDoc){
+            const doc = new Doc(item.id, item.name, undefined);
+            docs.push(doc);
+        }
+        else{
+            throw new MyError();
+        }
+    }
+}
+
+async function initGraph() {
+    const docs : Doc[] = [];
+    makeDocsFromDbFolder(docs, rootFolder!);
+
+    const sections : Section[] = [];
+    const edge_map = new Map<string, Edge>();
+
+    return [docs, sections, edge_map];
+    
+}
+
+async function loadGraph() : Promise<[Doc[], Section[], Map<string, Edge>]>{
+    if(rootFolder == null){
+        rootFolder = await makeRootFolder();
+    }
+
+    let graph_obj = await fetchDB("graph");
+
+    const doc_map = new Map<number, Doc>();
+    const section_map = new Map<number, Section>();
+
+    const parent_items : [number, MapItem][] = [];
+
+    for(const doc_obj of graph_obj.docs){
+        const doc = new Doc(doc_obj.id, doc_obj.title, undefined);
+        doc_map.set(doc.id, doc);
+
+        if(doc_obj.parent != -1){
+            parent_items.push([doc_obj.parent, doc]);
+        }
+    }
+
+    for(const sec_obj of graph_obj.sections){
+        const section = new Section(sec_obj.id, sec_obj.title, undefined);
+        section_map.set(section.id, section);
+
+        if(sec_obj.parent != -1){
+            parent_items.push([sec_obj.parent, section]);
+        }
+    }
+
+    for(const [parent_id, item] of parent_items){
+        const parent = section_map.get(parent_id);
+        if(parent == undefined){
+            msg(`unknown parent:${parent_id}`);
+            continue;
+        }
+
+        item.parent = parent;
+    }
+
+    const edge_map = new Map<string, Edge>();
+    for(const edge_obj of graph_obj.edges){
+        const src = doc_map.get(edge_obj.src);
+        const dst = doc_map.get(edge_obj.dst);
+        if(src == undefined || dst == undefined){
+            msg(`unknown src:${edge_obj.src} dst:${edge_obj.dst}`)
+            continue;
+        }
+
+        const edge = new Edge(src, dst);
+        const key = edge.key();
+        edge_map.set(key, edge);
+    }
+
+    const docs = Array.from(doc_map.values());
+    const sections = Array.from(section_map.values());
+
+    return [docs, sections, edge_map];
+}
+
+export async function makeDocsFromRootFolder(){
+    const map_div = document.createElement("div");
+    map_div.id = "map-div";
+    document.body.append(map_div);
+
+    const [docs, sections, edge_map] = await loadGraph();
+
+    graph = new Graph(docs, sections, edge_map);
+
+    $("remove-from-section").onclick = graph.removeFromSection.bind(graph);
+    $("connect-edge").addEventListener("click", graph.connectEdge.bind(graph));
+    $("update-graph").addEventListener("click", async (ev:MouseEvent)=>{
+        await updateGraph();
+    })
+    document.body.addEventListener("keydown", graph.onKeyDown.bind(graph));
+
+    graph.makeViz();        
+}
+
+export function makeDocsFromJson(data : any) : [Doc[], Section[], Map<string, Edge>] {
+    const sec_map = new Map<number, Section>();
+
+    const sections : Section[] = [];
+    if(data["sections"] != undefined){
+
+        const sec_to_parent_id : [Section, number][] = [];
+        for(const obj of data["sections"]){
+            const section = new Section(obj["id"], obj["title"], obj["wiki"]);
+            sections.push(section);
+            sec_map.set(section.id, section);
+
+            const parent_id = obj["parent"];
+            if(parent_id != undefined){
+
+                sec_to_parent_id.push([section, parent_id])
+            }
+        }
+
+        for(const [sec, parent_id] of sec_to_parent_id){
+            sec.parent = sec_map.get(parent_id);
+            if(sec.parent == undefined){
+
+                msg(`section parent is invalid:${sec.id} ${sec.title} parent:${parent_id}`);
+            }
+        }
+    }
+
+    const doc_map = new Map<number, Doc>();
+    for(const obj of data["docs"]){
+        const doc = new Doc(obj["id"], obj["title"], obj["wiki"]);
+
+        const parent_id = obj["parent"];
+        if(parent_id != undefined){
+            doc.parent = sec_map.get(parent_id);
+            if(doc.parent == undefined){
+
+                msg(`doc parent is invalid:${doc.id} ${doc.title} parent:${parent_id}`);
+            }
+        }
+
+        doc_map.set(doc.id, doc);
+    }
+    const docs = Array.from(doc_map.values());
+
+    const edge_map = new Map<string, Edge>();
+    for(const obj of data["edges"]){
+        const src_doc = doc_map.get(obj["src"])!;
+        const dst_doc = doc_map.get(obj["dst"])!;
+
+        assert(src_doc != undefined && dst_doc != undefined);
+
+        addEdge(edge_map, src_doc, dst_doc);
+    }    
+
+    const orphaned_docs = docs.filter(x => x.srcs.length == 0 && x.dsts.length == 0);
+    for(const doc of orphaned_docs){
+        msg(`orphaned doc:${doc.id} ${doc.title}`);
+        // remove(docs, doc);
+    }
+    for(const doc of docs){
+        const words = doc.title.split(":");
+        if(words.length < 4 || !words.every(x => x.length != 0)){
+            msg(`翻訳:${doc.title}`);
+        }
+    }
+
+    return [docs, sections, edge_map];
+}
+
+
+
+export class MapItem {
+    id       : number;
+    parent   : Section | undefined;
+    title    : string;
+    wiki     : string | undefined = undefined;
+    selected : boolean = false;
+
+    constructor(id : number, title : string, wiki : string | undefined){
+        this.id    = id;
+        this.title = title;
+        this.wiki  = wiki;
+    }
+
+    makeObj() : any {
+        const parent = (this.parent != undefined ? this.parent.id : -1);
+        return {
+            id : this.id,
+            parent,
+            title : this.title
+        };
+    }
+
+    localTitle() : string {
+        const titles = this.title.split(":");
+        if(langIdx < titles.length && titles[langIdx] != ""){
+            return titles[langIdx];
+        }
+        else{
+            return titles[0];
+        }
+    }
+
+    jsonStr() : string {
+        let wiki = this.wiki == undefined ? "" : `, "wiki":"${this.wiki}"`;
+
+        if(this.parent == undefined){
+
+            return `{ "id" : ${this.id}, "title" : "${this.title}"${wiki} }`;
+        }
+        else{
+
+            return `{ "id" : ${this.id}, "title" : "${this.title}", "parent" : ${this.parent.id}${wiki} }`;
+        }
+    }
+}
+
+export class Doc extends MapItem {
+    dsts  : Doc[] = [];
+    srcs  : Doc[] = [];
+
+    text! : SVGTextElement;
+    rect! : SVGRectElement;
+    ellipse : SVGEllipseElement | undefined;
+    textBox! : DOMRect;
+
+    width  : number = NaN;
+    height : number = NaN;
+
+    constructor(id : number, title : string, wiki : string | undefined){
+        super(id, title, wiki);
+    }
+
+    setColor(color : string){
+        this.rect.setAttribute("stroke", color);
+        this.text.setAttribute("stroke", color);
+    }
+
+    select(selected : boolean){
+        this.selected = selected;
+
+        let color = (this.selected ? "red" : "black");
+
+
+        if(this.text != undefined){
+            this.text.setAttribute("stroke", color);
+        }
+
+        if(this.rect != undefined){
+            this.rect.setAttribute("stroke", color);
+        }
+
+        if(this.ellipse != undefined){
+            this.ellipse.setAttribute("stroke", color);
+        }
+    }
+
+    selectSrcs(done:Doc[]){
+        if(done.includes(this)){
+            return;
+        }
+        done.push(this);
+        this.select(true);
+        for(const doc of this.srcs){
+            getEdge(edge_Map, doc, this)!.select(true);
+            doc.selectSrcs(done);
+        }
+    }
+
+    selectDsts(done:Doc[]){
+        if(done.includes(this)){
+            return;
+        }
+        done.push(this);
+        this.select(true);
+        for(const doc of this.dsts){
+            getEdge(edge_Map, this, doc)!.select(true);
+            doc.selectDsts(done);
+        }
+    }
+
+    onClick(ev : MouseEvent){        
+        msg("click doc");
+        if(ev.ctrlKey){
+        }
+        else{
+
+            this.selectSrcs([]);
+            this.selectDsts([]);
+        }
+    }
+
+    onVizClick(ev : MouseEvent){
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        if(ev.ctrlKey){
+
+            this.select(!this.selected);
+            if(this.selected && !graph.selections.includes(this)){
+                graph.selections.push(this);
+            }
+            else if(!this.selected && graph.selections.includes(this)){
+                remove(graph.selections, this);
+            }
+
+            msg(`viz: ${this.title}`);
+        }
+        else if(ev.shiftKey){
+            const new_url = `./movie.html?id=${this.id}`
+            msg(`open movie: ${this.id} ${this.title} url:${new_url}`);
+            window.open(new_url, "_blank");
+        }
+        else{
+            if(this.wiki != undefined){
+
+                const new_url = `https://en.wikipedia.org/wiki/${this.wiki}`;
+                msg(`open wiki: ${this.id} ${this.title} url:${new_url}`);
+                window.open(new_url, "_blank");
+            }
+        }
+    }
+
+    makeDot(lines : string[]){
+        const color = (this.wiki == undefined ? "black" : "blue");
+        lines.push(`b${this.id} [ label="${this.localTitle()}" id="${this.id}" class="doc" tooltip="　" fontsize="10" , fontcolor="${color}" ];` );
+    }
+}
+
+export class Edge {
+    src : Doc;
+    dst : Doc;
+    path! : SVGPathElement;
+    selected : boolean = false;
+
+    constructor(src : Doc, dst : Doc){
+        this.src = src;
+        this.dst = dst;
+    }
+
+    makeObj() : any {
+        return {
+            src : this.src.id,
+            dst : this.dst.id
+        }
+    }
+
+    key() : string {
+        return edgeKey(this.src, this.dst);
+    }
+
+    select(selected : boolean){
+        this.selected = selected;
+        if(this.selected){
+
+            this.path.setAttribute("stroke", "red");
+        }
+        else{
+
+            this.path.setAttribute("stroke", "black");
+        }
+    }
+
+    makeDot(lines : string[]){
+        let id = `${this.src.id}:${this.dst.id}`;
+        lines.push(`b${this.src.id} -> b${this.dst.id} [ id="${id}" ];`);
+    }
+
+    onEdgeClick(ev : MouseEvent){
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        if(ev.ctrlKey){
+
+            this.select(!this.selected);
+
+            msg(`edge click: ${this.src.title} => ${this.dst.title}`);
+        }
+    }
+
+    onEdgeMenu(ev : MouseEvent){
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        $("delete-edge").onclick = graph.deleteEdges.bind(graph);
+        // showDlg(ev, "graph-edge-menu-dlg");
+    }
+
+}
+
+export function makeIndexJson(docs : Doc[], sections : Section[], edges : Edge[]){
+    docs = docs.slice();
+
+    docs.sort((a:Doc, b:Doc) => a.id - b.id);
+
+    const lines : string[] = [];
+    lines.push(`{`);
+
+    lines.push(`    "docs" : [`);
+
+    for(const [i,doc] of docs.entries()){
+        const cm = (i == docs.length - 1 ? "" : ",");
+        lines.push(`        ${doc.jsonStr()}${cm}`);
+    }
+
+    lines.push(`    ]`);
+    lines.push(`    ,`);
+
+    lines.push(`    "sections" : [`);
+
+    for(const [i,section] of sections.entries()){
+        const cm = (i == sections.length - 1 ? "" : ",");
+        lines.push(`        ${section.jsonStr()}${cm}`);
+    }
+
+    lines.push(`    ]`);
+    lines.push(`    ,`);
+
+    lines.push(`    "edges" : [`);
+
+    edges.sort((a:Edge, b:Edge) => edgeKey(a.src, a.dst).localeCompare( edgeKey(b.src, b.dst) ));
+
+    for(const [i,edge] of edges.entries()){
+        const cm = (i == edges.length - 1 ? "" : ",");
+        lines.push(`        { "src" : ${edge.src.id}, "dst" : ${edge.dst.id} }${cm}`);
+    }
+
+    lines.push(`    ]`);
+    lines.push(`}`);
+
+    const text = lines.join("\n");
+
+    return text;
+}
+}
---
+++ ts/graph/graph.ts
@@ -0,0 +1,462 @@
+///<reference path="edge.ts" />
+declare var Viz: any;
+
+namespace firebase_ts {
+//
+let dlgSet = new Set<string>();
+export function showDlg(ev : MouseEvent, dlg_id : string){    
+    const dlg = $dlg(dlg_id);
+
+    dlg.style.left = `${ev.pageX}px`;
+    dlg.style.top  = ev.pageY + "px";
+
+    dlg.showModal();
+
+    if(!dlgSet.has(dlg_id)){
+        dlgSet.add(dlg_id);
+        dlg.addEventListener("click", (ev : MouseEvent)=>{
+            dlg.close();
+        });
+    }
+
+    // const rc = dlg.getBoundingClientRect();
+    // if(document.documentElement.scrollWidth < ev.pageX + rc.width){
+    //     dlg.style.left = `${document.documentElement.scrollWidth - rc.width}px`;
+    // }
+}
+
+
+let langList : string[] = [ "ja", "en", "ko", "zh" ];
+export let langIdx : number = 1;
+
+let showSubgraph : boolean = true;
+
+export class Graph {
+    docs : Doc[];
+    edgeMap = new Map<string, Edge>();
+    sections : Section[];
+    selections : Doc[] = [];
+
+    constructor(docs : Doc[], sections : Section[], edge_map : Map<string, Edge>){
+        this.docs  = docs;
+        this.sections = sections;
+        this.edgeMap = edge_map;
+    }
+
+    edges() : Edge[]{
+        return Array.from(this.edgeMap.values());
+    }
+
+    makeViz(){
+        let doc_map = new Map<string, Doc>();
+        this.docs.forEach(doc => doc_map.set(`${doc.id}`, doc));
+
+        let sec_map = new Map<string, Section>();
+        this.sections.forEach(sec => sec_map.set(`s${sec.id}`, sec));
+
+        let docLines  : string[] = [];
+        let secLines  : string[] = [];
+        let edgeLines : string[] = [];
+
+        if(showSubgraph){
+            this.docs.filter(doc => doc.parent == undefined).forEach(doc => doc.makeDot(docLines));
+            this.sections.filter(sec => sec.parent == undefined).forEach(sec => sec.makeDot(secLines));
+        }
+        else{
+            this.docs.forEach(doc => doc.makeDot(docLines));
+        }
+
+        this.edges().forEach(edge => edge.makeDot(edgeLines));
+
+        const ranks : string[] = [];
+
+        let dot = `
+        digraph graph_name {
+            graph [
+                rankdir = "TB";
+                charset = "UTF-8";
+            ];
+            ${docLines.join('\n')}
+            ${secLines.join('\n')}
+            ${edgeLines.join('\n')}
+            ${ranks.join('\n')}
+        }
+        `;
+
+        Viz.instance().then(function(viz:any) {
+            var svg = viz.renderSVGElement(dot) as SVGSVGElement;
+
+            svg.addEventListener("contextmenu", onMenu);
+            svg.addEventListener("click", graph.onClick.bind(graph));
+
+            const nodes = Array.from(svg.getElementsByClassName("node doc")) as SVGGElement[];
+            for(const g of nodes){
+                const doc = doc_map.get(g.id);
+                if(doc == undefined){
+
+                    msg(`node NG: ${g.id} [${g.textContent}]`);
+                }
+                else{
+                    g.addEventListener("click", doc.onVizClick.bind(doc));
+
+                    const ellipses = g.getElementsByTagName("ellipse");
+                    if(ellipses.length == 1){
+                        doc.ellipse = ellipses.item(0)!;
+                    }
+                }
+
+                g.setAttribute("cursor", "pointer");
+            }
+
+
+            const edges = Array.from(svg.getElementsByClassName("edge")) as SVGGElement[];
+            for(const g of edges){
+                const edge = graph.edgeMap.get(g.id);
+                if(edge == undefined){
+
+                    msg(`edge NG: ${g.id} [${g.textContent}]`);
+                }
+                else{
+                    g.addEventListener("click", edge.onEdgeClick.bind(edge));
+                    g.addEventListener("contextmenu", edge.onEdgeMenu.bind(edge));
+
+                    const paths = g.getElementsByTagName("path");
+                    if(paths.length == 1){
+                        edge.path = paths.item(0)!;
+                    }
+                    else{
+                        msg(`edge no path: ${g.id} ${paths.length} [${g.textContent}]`);                        
+                    }
+                }
+
+                g.setAttribute("cursor", "crosshair");
+            }
+
+            if(showSubgraph){
+
+                for(const [id, sec] of sec_map.entries()){
+                    const g = svg.getElementById(id) as SVGGElement;
+                    assert(g != undefined);
+                    g.addEventListener("click", sec.onSectionClick.bind(sec));
+                    g.addEventListener("contextmenu", sec.onSectionMenu.bind(sec));
+
+                    const polygons = g.getElementsByTagName("polygon");
+                    if(polygons.length == 1){
+                        sec.polygon = polygons.item(0)!;
+                    }
+
+                    g.setAttribute("cursor", "pointer");
+                }
+            }
+
+            const map_div = $("map-div");
+            map_div.innerHTML = "";
+
+            map_div.appendChild(svg);
+
+            const rc = svg.getBoundingClientRect();
+
+            map_div.style.width = `${rc.width.toFixed()}px`;
+            map_div.style.height = `${rc.height.toFixed()}px`;
+        });
+    }
+
+
+    async onKeyDown(ev : KeyboardEvent){        
+        if(ev.key == "Escape"){
+            this.clearSelections();
+        }
+        else if(ev.key == "Delete"){
+            await this.deleteEdges();
+        }
+    }
+
+    async deleteEdges(){
+        const selected_edges = this.edges().filter(edge => edge.selected);
+        if(selected_edges.length != 0){
+
+            if(window.confirm("Do you really want to delete?")) {
+                selected_edges.forEach(edge => this.edgeMap.delete(edge.key()));
+
+                this.clearSelections();
+
+                await updateGraph();
+            }
+        }
+    }
+
+    clearSelections(){
+        this.docs.filter(doc => doc.selected).forEach(doc => doc.select(false));
+        this.selections = [];
+        
+        this.edges().filter(edge => edge.selected).forEach(edge => edge.select(false));
+    }
+
+    addDoc(title : string, wiki : string | undefined) : Doc {
+        let next_id = 1;
+        for(const doc of this.docs){
+            if(next_id < doc.id){
+                break;
+            }
+            next_id++;
+        }
+        const doc = new Doc(next_id, title, wiki);
+        this.docs.push(doc);
+
+        this.docs.sort((a:Doc, b:Doc) => a.id - b.id);
+
+        return doc;
+    }
+
+    addSection(title : string, wiki : string | undefined) : Section{
+        let next_id = 1;
+        for(const sec of this.sections){
+            if(next_id < sec.id){
+                break;
+            }
+            next_id++;
+        }
+
+        const section = new Section(next_id, title, wiki);
+        this.sections.push(section);
+
+        this.docs.filter(x => x.selected).forEach(x => x.parent = section);
+        this.clearSelections();
+
+        return section;
+    }
+
+    async removeFromSection(ev : MouseEvent){
+        this.docs.filter(x => x.selected).forEach(x => x.parent = undefined);
+        this.clearSelections();
+
+        await updateGraph();
+    }
+
+    async connectEdge(ev : MouseEvent){
+        if(this.selections.length < 2){
+            return;
+        }
+
+        for(let i = 0; i + 1 < this.selections.length; i++){
+            const [src, dst] = this.selections.slice(i, i + 2);
+
+            if(getEdge(this.edgeMap, src, dst) == undefined){
+                addEdge(this.edgeMap, src, dst);
+            }
+        }
+
+        this.clearSelections();
+
+        await updateGraph();
+    }
+
+    onClick(ev : MouseEvent){
+        this.docs.filter(x => x.selected).forEach(x => x.select(false));
+    }
+}
+
+export let graph : Graph;
+
+export class Section extends MapItem {
+    polygon : SVGPolygonElement | undefined;
+
+    constructor(id: number, title : string, wiki : string | undefined){
+        super(id, title, wiki);
+    }
+
+    select(selected : boolean){
+        this.selected = selected;
+
+        let color = (this.selected ? "red" : "black");
+
+        if(this.polygon != undefined){
+            this.polygon.setAttribute("stroke", color);
+        }
+    }
+
+    onSectionClick(ev : MouseEvent){
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        if(ev.ctrlKey){
+
+            this.select(!this.selected);
+            msg(`section : ${this.title}`);
+        }
+    }
+
+    onSectionMenu(ev : MouseEvent){
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        $("add-section-to-section").onclick = this.addSectionToSection.bind(this);
+        $("add-item-to-section").onclick = this.addItemToSection.bind(this);
+        $("append-to-section").onclick = this.appendToSection.bind(this);
+        showDlg(ev, "graph-section-menu-dlg");
+    }
+
+    async addItemToSection(ev : MouseEvent){
+        const title = prompt();
+        if(title == null){
+            return;
+        }
+
+        const doc = graph.addDoc(title, undefined);
+        doc.parent = this;
+
+        await updateGraph();
+    }
+
+    async addSectionToSection(ev : MouseEvent){
+        const title = prompt();
+        msg(`input ${title}`);
+        if(title == null){
+            return;
+        }
+        const sec = graph.addSection(title, undefined);
+        sec.parent = this;
+
+        await updateGraph();
+    }
+
+    async appendToSection(ev : MouseEvent){
+        msg(`append To Section`);
+        allMapItems().filter(x => x.selected).forEach(x => x.parent = this);
+        graph.clearSelections();
+
+        await updateGraph();
+    }
+
+    makeDot(lines : string[]){
+        lines.push(`subgraph cluster_${this.id} {`);
+        lines.push(`    id = "s${this.id}";`);
+        lines.push(`    label = "${this.localTitle()}";`);
+        lines.push(`    labelloc  = "b";`);
+        lines.push(`    labeljust = "l";`);
+        lines.push(`    bgcolor   = "cornsilk";`);
+        lines.push(`    color     = "green";`);
+        lines.push(`    penwidth  = 2;`);
+
+        graph.sections.filter(sec => sec.parent == this).forEach(sec => sec.makeDot(lines));
+        const docs = graph.docs.filter(sec => sec.parent == this);
+        if(docs.length == 0){
+            // If there is no children, subgraph is not created.
+            
+            lines.push(`b${this.id}dummy [ label="dummy" style="invis" ];` );
+        }
+        else{
+            docs.forEach(doc => doc.makeDot(lines));
+        }
+
+        lines.push(`}`);
+    }
+}
+
+
+async function onMenu(ev : MouseEvent){
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    showDlg(ev, "graph-menu-dlg");
+}
+
+function setLangHandler(){
+    const lang_buttons = Array.from(document.getElementsByClassName("lang")) as HTMLButtonElement[];
+    for(const lang_button of lang_buttons){
+        lang_button.addEventListener("click", (ev: MouseEvent)=>{
+            const lang = lang_button.id.substring(5);
+            langIdx = langList.indexOf(lang);
+            msg(`lang:${langIdx} ${lang}`);
+            if(langIdx == undefined){
+                throw new MyError();
+            }
+
+            graph.makeViz();
+        })
+
+    }
+
+}
+
+function getSearchParams(){
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+
+    const lang = params.get("lang");
+    if(lang != undefined){
+        const lang_idx = langList.indexOf(lang);
+        if(lang_idx != -1){
+            langIdx = lang_idx;
+        }
+    }
+}
+
+export async function bodyOnLoadGraph(){
+    getSearchParams();
+
+    setLangHandler();
+
+    $("map-svg").style.display = "none";
+
+/*
+    const data = await fetchJson(`../data/graph.json`);
+
+    const [docs, sections, edge_map] = makeDocsFromJson(data);
+
+*/
+}
+
+export async function updateGraph(){
+    graph.makeViz();        
+
+    if(user == null || rootFolder == null || refId == undefined){
+        throw new MyError();
+    }
+
+    const graph_obj = {
+        version : 1.0,
+        docs : graph.docs.map(x => x.makeObj()),
+        sections : graph.sections.map(x => x.makeObj()),
+        edges : graph.edges().map(x => x.makeObj())
+    };
+
+    try{
+        await getDocRef("graph").set(graph_obj);
+        msg(`update graph [${JSON.stringify(graph_obj, null, 4)}]`);
+    }
+    catch(e){
+        msg(`update graph error: ${user.email} ref:${refId} ${e}`);
+    }
+}
+
+export async function addGraphItem(){
+    const title = prompt();
+    msg(`input ${title}`);
+    if(title == null){
+        return;
+    }
+    graph.addDoc(title, undefined);
+    await updateGraph();
+}
+
+export async function addGraphSection(){
+    const title = prompt();
+    msg(`input ${title}`);
+    if(title == null){
+        return;
+    }
+    graph.addSection(title, undefined);
+    await updateGraph();
+}
+
+export async function changeDisplay(){
+    showSubgraph = ! showSubgraph;
+    graph.makeViz();      
+}
+
+function allMapItems() : MapItem[] {
+    return (graph.docs as MapItem[]).concat(graph.sections);
+}
+
+}
